86b1006e188553fc657f3e0ee810038a7b9bd858
Senem Yilmaz <senemyilmaaz@outlook.com>
2025-12-16 13:49:34 +0300

    Add docstrings and fix pylint warnings

--- today.py
+++ today.py
@@ -1,10 +1,13 @@
-'''
-This is the module docstring
-'''
 
-
-pass  # YOUR CODE HERE
 def my_name_is():
+    """
+    Returns my name.
+    """
     return "Senem"
+
+
 def my_buddy_is():
+    """
+    Returns my buddy's name.
+    """
     return "Workintech"
